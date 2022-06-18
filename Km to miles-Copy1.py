5928e1fd8449f6216b299b73067b551c9442cd79
VyshakR <105199949+VyshakR@users.noreply.github.com>
2022-06-18 10:16:51 +0530

    Add files via upload

---
+++ Km to miles-Copy1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def kilometre_1(km):  
+    conversion_ratio_1= 0.621371  
+    miles_1 = km * conversion_ratio_1  
+    print ("The speed value of car in Miles: ", miles_1)  
+km = float (input ("Please enter the speed of car in Kilometre as a unit: "))  
+kilometre_1(km) 
+
+
+# In[ ]:
+
+
+
+
